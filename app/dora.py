406733d23e0c803c89f80927a304498b65a0a237
Caian R. Ertl <caianrais@protonmail.com>
2017-11-02 21:35:05 -0200

    Class `Resolver`: Implement basic structure and functionality
    The class `Resolver` implements the usage of `dnspython`'s DNS query
    functionality. It's not yet tested and will be expanded with new query
    types.

--- app/dora.py
+++ app/dora.py
@@ -64,9 +64,83 @@ class ResolverException:
 # Resolver
 #------------------------------
 class Resolver:
+    """
+    """
 
-    def __init__(self):
+    _domain = None
+    _record = None
+    _response = None
+    _resolver = None
+
+    def __init__(self, domain, record):
+        """
+        """
+        self._domain = domain
+        self._record = record
+        self._response = []
+        self._resolver = dns.resolver
+
+    def look(self):
+        """
+        """
+        if self._domain == None or self._record == None:
+            pass
+
+        elif self._record == 'A':
+            self._dig_a
+
+        elif self._record == 'MX':
+            self._dig_mx
+
+        elif self._record == 'NS':
+            self._dig_ns
+
+        elif self._record == 'TXT':
+            self._dig_txt
+
+        else:
+            pass
+
+    def _dig_a(self):
         pass
+
+    def _dig_mx(self):
+        mx_query = self._resolver.query(self._domain, self._record)
+
+        for mx_data in mx_query:
+            mx_item_hostname = str(mx_data.exchange)
+            mx_item_priority = mx_data.preference
+
+            self._result.append({
+                'hostname': mx_item_hostname,
+                'priority': mx_item_priority
+                })
+
+        return self._response
+
+    def _dig_ns(self):
+        ns_query = self._resolver.query(self._domain, self._record)
+        ns_answer = ns_query.response.answer
+
+        for ns_data in ns_answer:
+            for ns_item in ns_data.items:
+                self._response.append({
+                    'nameserver': ns_item.to_text()
+                    })
+
+        return self._response
+
+    def _dig_txt(self):
+        txt_query = self._resolver.query(self._domain, self._record)
+        txt_answer = txt_query.response.answer
+
+        for txt_data in txt_answer:
+            for txt_item in txt_data.items:
+                self._response.append({
+                    'text': txt_item.to_text()
+                    })
+
+        return self._response
 
 
 application = Flask(__name__)
