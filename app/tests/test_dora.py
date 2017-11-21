6f41782fd0bc1fd975426b1f99a8bbfad536dc0e
Caian R. Ertl <caianrais@protonmail.com>
2017-11-21 06:43:45 -0200

    Travis-CI: Fixes and additions
     - Fixes Flask's object name definition.
       Went broken because of commit #9e9addf
    
     - Adds a invalid endpoint test.

--- app/tests/test_dora.py
+++ app/tests/test_dora.py
@@ -2,11 +2,22 @@ import sys
 from os import path
 
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-import dora
 
-def test_splash():
-    dora.application.testing = True
-    client = dora.application.test_client()
+import dora as application
+import pytest
 
+
+@pytest.fixture
+def client():
+    application.dora.testing = True
+    return application.dora.test_client()
+
+
+def test_splash(client):
     response = client.get('/')
     assert response.status_code == 200
+
+
+def test_invalid_endpoint(client):
+    response = client.get('/an-invalid-endpoint')
+    assert response.status_code == 404
